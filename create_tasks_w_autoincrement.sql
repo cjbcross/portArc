30afed835418920c7fab82f95edeb32dca4ded7e
Chris Butler <cjbcross@gmail.com>
2018-03-25 15:05:37 -0400

    Adding the almost completed WAR and a create script for tasks

---
+++ create_tasks_w_autoincrement.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE TABLE public.tasks
+(
+  choice character varying(100),
+  folder character varying(400),
+  task_id BIGSERIAL PRIMARY KEY,
+  version integer,
+  selected_time_stamp integer,
+  completed_time_stamp integer,
+  removed_time_stamp integer,
+  username character varying(40)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.tasks
+  OWNER TO postgres;
