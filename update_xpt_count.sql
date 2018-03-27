d794d4da6e041e9a1b3d12465d14a4ff2bd1df4e
Chris Butler <cjbcross@gmail.com>
2018-03-27 12:57:09 -0400

    Final WAR and update_xpt script

--- update_xpt_count.sql
+++ update_xpt_count.sql
@@ -4,13 +4,7 @@ FROM (
 SELECT COUNT(file_metadata.filename) as xpt_count, folders.folder as folder
 FROM file_metadata, folders 
 WHERE file_metadata.file_href LIKE folders.folder || '%'
-AND file_metadata.file_type = 'xpt'
+AND (file_metadata.file_type LIKE '%xpt%' OR file_metadata.file_type LIKE '%csv%' OR file_metadata.file_type LIKE '%sas7bdat%' OR file_metadata.file_type LIKE '%xls%')
 GROUP BY folders.folder 
 ) AS sub
 WHERE folders.folder=sub.folder;
-
-
-
-
-
-
