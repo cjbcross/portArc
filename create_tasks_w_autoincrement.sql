f0b376c334f4769f00647effd6755bb2b7b543ff
Chris Butler <cjbcross@gmail.com>
2018-03-26 22:26:39 -0400

    re-upload of create_tasks script

--- create_tasks_w_autoincrement.sql
+++ create_tasks_w_autoincrement.sql
@@ -1,2 +1,16 @@
-﻿
-select user_tasks.choice, tasks.folder, tasks.selected_time_stamp from user_tasks, tasks where user_tasks.user_tasks_id = tasks.choice;
+﻿CREATE TABLE public.tasks
+(
+  choice character varying(100),
+  folder character varying(400),
+  task_id BIGSERIAL PRIMARY KEY,
+  version integer,
+  selected_time_stamp integer,
+  completed_time_stamp integer,
+  removed_time_stamp integer,
+  username character varying(40)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.tasks
+  OWNER TO postgres;
